b1bfd72f6b45039bb1dfad556e0d6215e795c1d1
nguyenbvhe <buivannguyen1312@gmail.com>
2025-11-19 10:32:12 +0700

    Initial Commit

---
+++ CodeChallengeTests/CodeChallengeTests.swift
@@ -0,0 +1,17 @@
+//
+//  CodeChallengeTests.swift
+//  CodeChallengeTests
+//
+//  Created by minhnguyen on 19/11/25.
+//
+
+import Testing
+@testable import CodeChallenge
+
+struct CodeChallengeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
